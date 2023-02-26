3cfba7355cca5e9d686a393f32c8b39304ebfb9e
= <BrandonGong@users.noreply.github.com>
2023-02-26 14:33:54 -0700

    Changed shared variables to session storage

--- webapp.py
+++ webapp.py
@@ -1,50 +1,60 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, session
+import secrets
 from cookbook import Cookbook
 from pyvis.network import Network
 
 app = Flask(__name__)
+app.secret_key = secrets.token_hex()
 cb =  Cookbook()
 
-_active_effects = []
 
 @app.route('/')
 def Home():
+    active_effects = _GetActiveEffects()
     effects = []
     for effect in cb.AllEffects():
         e = {}
         e["name"] = effect
-        e["active"] = effect in _active_effects
+        e["active"] = effect in active_effects
         e["compatible"] = True
-        for ae in _active_effects:
+        for ae in active_effects:
             if effect not in cb.GetCompatibleEffects(ae):
                 e["compatible"] = False
                 break
 
         effects.append(e)
     
-    return render_template("index.html", effects=effects, active_effect=_active_effects )
+    return render_template("index.html", effects=effects)
 
 @app.route('/AddEffect',methods=['POST'])
 def AddEffect():
-    _active_effects.append(request.form["effect"])
+    if "active_effects" in session:
+        session["active_effects"].append(request.form["effect"])
+        session.modified = True
+    else:
+        session["active_effects"] = [request.form["effect"]]
+
     return redirect('/')
 
 
 @app.route("/Clear",methods=["POST"])
 def Clear():
-    _active_effects.clear()
+    if "active_effects" in session:
+        session["active_effects"].clear()
+        session.modified = True
     return redirect('/')
 
 @app.route("/Graph")
 def Graph():
-    if len(_active_effects) > 0:
+    active_effects = _GetActiveEffects()
+    if len(active_effects) > 0:
         # Max ingredients per potion is 3
         # For potions with only 2 ingredients to have an effect
         # both ingredients must share an effect.
         # For multiple effects, all effects must be shared between two ingredients, or for
         # potions with 3 ingredients they must be linked by an ingredient with both effects.
         g = Network(height="750px",bgcolor="#222222",font_color="#f2f2f2")
-        for effect in _active_effects:
+        for effect in active_effects:
             g.add_node(effect,color="#0d6efd",shape="star")
             ingredients = cb.GetIngredients(effect)
             for ingredient in ingredients:
@@ -54,7 +64,7 @@ def Graph():
                 g.add_edge(effect,ingredient)
 
                 # for e in secondary_effects:
-                #     if e not in _active_effects:
+                #     if e not in active_effects:
                 #         # add a secondary effect
                 #         g.add_node(e,color="#adb5bd",shape="star")
                 #         g.add_edge(e,ingredient)
@@ -63,6 +73,13 @@ def Graph():
         return g.generate_html()
     else:
         return "Select an effect to view ingredients."
+
+
+def _GetActiveEffects():
+    if "active_effects" in session:
+        return session["active_effects"]
+    else:
+        return []
 
 if __name__ == "__main__":
     app.run()
